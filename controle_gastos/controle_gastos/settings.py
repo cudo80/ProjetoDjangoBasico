191c3104396afed9cc8ed8c753bc026466682fa0
Luiz Henrique Cudo <86842391+cudo80@users.noreply.github.com>
2021-12-02 09:26:22 -0300

    Update settings.py

--- controle_gastos/controle_gastos/settings.py
+++ controle_gastos/controle_gastos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-361affpm+xt7^i!4kry40wb%6h^x-=z4rjya0!i4hdgz%5@o(p'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
